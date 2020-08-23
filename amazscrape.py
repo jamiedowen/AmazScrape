2cd7bb754d9648a4cd68fd6f204fd805e28b939a
Jamie Dowen <jamiedowen@gmail.com>
2020-08-23 15:29:18 +0100

    revision 0.3

--- amazscrape.py
+++ amazscrape.py
@@ -4,21 +4,25 @@ import time
 import requests
 import smtplib
 from bs4 import BeautifulSoup
+from datetime import datetime
 
 #Define product URL, user agent and wanted price
 URL = "https://www.amazon.co.uk/dp/B081QZPSG8/ref=dp_cerb_2"
 HEADERS = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:79.0) Gecko/20100101 Firefox/79.0"}
 WANTED_PRICE = 810
 
+now = datetime.now()
+
+print(now)
 def trackPrice():
     price = float(getPrice())
     if price > WANTED_PRICE:
-        diffhigh = price - WANTED_PRICE
+        diffHigh = price - WANTED_PRICE
         print(f"Waiting for item to reach £{WANTED_PRICE}")
-        print(f"It's still £{diffhigh:1.2f} too expensive today")
+        print(f"It's still £{diffHigh:1.2f} too expensive today")
     else:
-        difflow = WANTED_PRICE - price
-        print(f"It's £{difflow:1.2f} cheaper than £{WANTED_PRICE} today")
+        diffLow = WANTED_PRICE - price
+        print(f"It's £{diffLow:1.2f} cheaper than £{WANTED_PRICE} today")
 
 #Request
 def getPrice():
